f3fc9f22a4ca3fd54f725228380ff22c5d1145b7
Annada Behera <annada.behera@niser.ac.in>
2024-11-09 14:04:55 +0530

    Added new classes: Camera, Film, Mesh, Light

--- TheRenderEngine/__init__.py
+++ TheRenderEngine/__init__.py
@@ -1,3 +1,44 @@
+from constants import *
+
+class Film:
+    ''' The film stores the image after it has been rendered.
+    '''
+class Mesh:
+    ''' The meshes in the scene. '''
+class Light:
+    ''' The lights in the scene. '''
+class Camera:
+    '''
+    The Camera is when added to a scene makes it renderable. Without a Camera
+    a scene cannot be rendered.
+    '''
+
+    def __init__(self, position, direction, film: Film,
+    up=[0, 1, 0], projection=PERSPECTIVE, clip_start=.1, clip_end=100):
+        '''The Camera constuctor. Creates a new camera.
+        @param position The position of the camera in the World.
+        @param direction The direction the camera is pointing at.
+        @param film The camera's film which will store the image.
+        @param up The up-axis of the camera. Defaults to Y-up, i.e, [0, 1, 0]. No
+        matter the values, the vector will be normalized before it is used.
+        @param projection The camera's projection, can be either `PERSPECTIVE` or `ORTHOGRAPHIC`. Defaults to `PERSPECTIVE`
+        @param clip_start The nearest cliping distance.
+        @param clip_end The nearest cliping distance.
+        '''
+        pass
+        # TODO H: Create internal variables for the above parameters so that we
+        # can use it.
+    def __repr__(self,):
+        ''' Returns a representation of the camera.'''
+        pass
+        # TODO H: Add the camera's info string.
+
+    def _render(self, world):
+        ''' The main rendering takes place here and the result is stored
+        in the film
+        '''
+        pass
+    
 class World:
     '''The World encompasses the entire scene, including all the
     object such as mesh, lights, and essentially contain atleast one
@@ -22,7 +63,7 @@ class World:
         # TODO H: Return string of all the lights camera and meshes in
         # current scene.
 
-    def add_mesh(self, mesh):
+    def add_mesh(self, mesh: Mesh):
         '''! Adds a mesh to the world.
          @param mesh The mesh to be added.
          @return Nothing is returned.
@@ -31,7 +72,7 @@ class World:
         # TODO H: Create a Mesh class.
         # TODO A: Check if the input is a Mesh and add it to self._meshes
 
-    def add_light(self, light):
+    def add_light(self, light: Light):
         '''! Adds a light to the world.
         @param light The light to be added.
         @return Nothing is returned.
@@ -40,7 +81,7 @@ class World:
         # TODO H: Create a Light class.
         # TODO A: Check if the input is a Light and add it to self._lights
  
-    def add_camera(self, camera):
+    def add_camera(self, camera: Camera):
         '''! Adds a Camera to the world.
         @param camera The camera to be added.
         @return Nothing is returned.
@@ -49,10 +90,11 @@ class World:
         # TODO A: Create a Camera class.
         # TODO H: Check if the input is a Camera and add it to self._cameras
 
-    def render(self, camera='main'):
+    def render(self, camera:Camera='main'):
         '''! Renders the current scene
-        @param camera_idx The index of the camera which should be used to render
-        the scene. Defaults to the 'main' camera added to the scene.
+        @param camera The index of the camera which should be used to render
+        the scene. Defaults to the 'main' camera added to the scene. Returns
+        an error is there is no 'main' camera in the scene.
         @return Nothing. The result of the renderer is present in the
         camera film.
         '''
