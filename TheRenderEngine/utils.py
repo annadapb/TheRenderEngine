1d89e804685c38b98eb633c5565effc68e65b0fd
Annada Behera <annada.behera@niser.ac.in>
2024-11-12 12:33:40 +0530

    Added Vec3, Color and Ray classes.

--- TheRenderEngine/utils.py
+++ TheRenderEngine/utils.py
@@ -1,3 +1,27 @@
+class Vec3:
+    ''' The class to store 3D vectors. '''
+    def __init__(self, x: float, y:float, z:float):
+        ''' The vector constructor. Use * to expand pass a list.
+        @param x The x component
+        @param y The y component
+        @param z The z component
+        '''
+        pass
+
+    def norm(self,) -> float:
+        ''' Returns the norm (length of the vector) without changing the vector.
+        @return The norm (length) of the vector.
+        '''
+        pass
+
+    def norm_(self,) -> float:
+        ''' Returns the norm (length of the vector) while also normalizing it.
+        @return The norm (length) of the vector.
+        '''
+        pass
+
+
+
 class Color:
     ''' A color object that stores a color '''
 
